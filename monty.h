f3d7a92dfd41c7e664ca39208a2d4d974b906604
SpeedCash <tcravero@gmail.com>
2024-01-03 14:06:53 +0000

    monthy upadate pop

--- monty.h
+++ monty.h
@@ -38,6 +38,8 @@ void pall(stack_t **stack, unsigned int line_number);
 void push(stack_t **stack, unsigned int line_number, const char *arg);
 void free_stack(stack_t **stack);
 void pint(stack_t **stack, unsigned int line_number);
+void pop(stack_t **stack, unsigned int line_number);
+
 
 
 #endif
