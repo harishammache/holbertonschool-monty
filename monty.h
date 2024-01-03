7df9e24883d760b101f85d364d2e03c70e7c8326
SpeedCash <tcravero@gmail.com>
2024-01-03 13:58:43 +0000

    struct stack_s - doubly linked list representation of a stack (or queue)

--- monty.h
+++ monty.h
@@ -37,5 +37,7 @@ typedef struct instruction_s
 void pall(stack_t **stack, unsigned int line_number);
 void push(stack_t **stack, unsigned int line_number, const char *arg);
 void free_stack(stack_t **stack);
+void pint(stack_t **stack, unsigned int line_number);
+
 
 #endif
