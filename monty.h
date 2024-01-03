1864b916d7475431ae33356935d29d90a6623320
SpeedCash <tcravero@gmail.com>
2024-01-03 08:26:27 +0000

    opcode and its function

--- monty.h
+++ monty.h
@@ -32,5 +32,8 @@ typedef struct instruction_s
 		void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
 
+void pall(stack_t **stack, unsigned int line_number);
+void push(stack_t **stack, unsigned int line_number, const char *arg);
+
 #endif
 
