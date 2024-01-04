8d7b9cb3e69ffc8f0110af1143b04f60b2536c2f
harishammache <hammache_haris@hotmail.fr>
2024-01-04 08:23:43 +0000

    Implement the nop opcode

---
+++ nop.c
@@ -0,0 +1,11 @@
+#include "monty.h"
+/**
+ * nop - Implement the nop opcode
+ * @stack: don't use this pointeur
+ * @line_number: don't use this integer
+*/
+void nop(stack_t **stack, unsigned int line_number)
+{
+	(void)stack;
+	(void)line_number;
+}
