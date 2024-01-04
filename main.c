8e6f7890a71d7d7e9ed66f4fd822e541f0352354
SpeedCash <tcravero@gmail.com>
2024-01-04 08:35:39 +0000

    updated main

--- main.c
+++ main.c
@@ -74,6 +74,10 @@ int main(int argc, char *argv[])
 		{
 			add(&stack, line_number);
 		}
+		else if (strcmp(opcode, "nop") == 0)
+		{
+			nop(&stack, line_number);
+		}
 		else
 		{
 			fprintf(stderr, "L%d: unknown instruction %s\n", line_number, opcode);
