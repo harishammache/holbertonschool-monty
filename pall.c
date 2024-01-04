0df9abe790463cd84e8da07c77d9c6c8721b726b
SpeedCash <tcravero@gmail.com>
2024-01-04 08:59:13 +0000

    updated pall

--- pall.c
+++ pall.c
@@ -3,19 +3,11 @@
 #include <stdio.h>
 #include <string.h>
 
-/*
- * pall.c
- *
- * Description:
- *     Ce fichier contient la fonction pall qui est utilisée pour imprimer
- *     tous les éléments d'une pile. L'impression commence à
- *partir du sommet
- *     de la pile.
- *
- *     Cette fonction parcourt la pile et imprime chaque élément. Si la pile
- *     est vide, elle ne fait rien. C'est un outil utile pour visualiser le
- *     contenu de la pile à un moment donné, particulièrement utile pour le
- *     débogage et la vérification de l'état actuel de la pile.
+/**
+ * pall - Implement pall opcodes.
+ * @stack: A pointer to a pointer to the stack (a doubly linked list)
+ * @line_number: The line number in the Monty bytecode
+ * file where the "pall" opcode is encountere
  */
 
 void pall(stack_t **stack, unsigned int line_number)
