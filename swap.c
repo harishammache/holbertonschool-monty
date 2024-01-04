9e033fb2169ba2a08970002f18b4deb194c15f87
harishammache <hammache_haris@hotmail.fr>
2024-01-04 08:46:17 +0000

    add description for the arguments

--- swap.c
+++ swap.c
@@ -1,25 +1,30 @@
 #include "monty.h"
-
+/**
+ * swap - Implement the swap opcode
+ * @stack: A pointer to a pointer to the stack (a doubly linked list)
+ * @line_number: The line number in the Monty bytecode
+ * file where the "swap" opcode is encountere
+*/
 void swap(stack_t **stack, unsigned int line_number)
 {
-    stack_t *first, *second;
+	stack_t *first, *second;
 
-    if (stack == NULL || *stack == NULL || (*stack)->next == NULL)
-    {
-        fprintf(stderr, "L%d: can't swap, stack too short\n", line_number);
-        exit(EXIT_FAILURE);
-    }
+	if (stack == NULL || *stack == NULL || (*stack)->next == NULL)
+	{
+		fprintf(stderr, "L%d: can't swap, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
 
-    first = *stack;
-    second = (*stack)->next;
+	first = *stack;
+	second = (*stack)->next;
 
-    first->next = second->next;
-    first->prev = second;
-    second->next = first;
-    second->prev = NULL;
+	first->next = second->next;
+	first->prev = second;
+	second->next = first;
+	second->prev = NULL;
 
-    if (first->next != NULL)
-        first->next->prev = first;
+	if (first->next != NULL)
+		first->next->prev = first;
 
-    *stack = second;
+	*stack = second;
 }
