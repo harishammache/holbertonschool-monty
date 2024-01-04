84d200dd3ac08cae7370c14445d3097e2ff7916d
harishammache <hammache_haris@hotmail.fr>
2024-01-04 11:56:51 +0000

    add condition

--- push.c
+++ push.c
@@ -20,8 +20,7 @@
 
 void push(stack_t **stack, unsigned int line_number, const char *arg)
 {
-	stack_t *new;
-	int num;
+	int i = 0;
 
 	if (arg == NULL)
 	{
@@ -29,19 +28,32 @@ void push(stack_t **stack, unsigned int line_number, const char *arg)
 		exit(EXIT_FAILURE);
 	}
 
-	num = atoi(arg);
+	for (i = 0; arg[i]; i++)
+	{
+		if (!isdigit(arg[i]) && !(i == 0 && arg[i] == '-'))
+		{
+			fprintf(stderr, "L%d: usage: push integer\n", line_number);
+			exit(EXIT_FAILURE);
+		}
+	}
 
-	new = malloc(sizeof(stack_t));
-	if (new == NULL)
+	int n = atoi(arg);
+
+	stack_t *new_node = malloc(sizeof(stack_t));
+
+	if (!new_node)
 	{
 		fprintf(stderr, "Error: malloc failed\n");
 		exit(EXIT_FAILURE);
 	}
 
-	new->n = num;
-	new->prev = NULL;
-	new->next = *stack;
+	new_node->n = n;
+	new_node->prev = NULL;
+	new_node->next = *stack;
+
 	if (*stack != NULL)
-		(*stack)->prev = new;
-	*stack = new;
+	{
+		(*stack)->prev = new_node;
+	}
+	*stack = new_node;
 }
