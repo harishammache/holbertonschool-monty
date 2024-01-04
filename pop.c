a28fbc9667cea6cb6fd0e48e7e6b56c076c0df0a
SpeedCash <tcravero@gmail.com>
2024-01-04 08:53:54 +0000

    updated pop

--- pop.c
+++ pop.c
@@ -8,12 +8,14 @@
  * Description:
  *     Cette fonction est conçue pour exécuter l'opcode 'pop'.
  *     Elle retire l'élément se trouvant au sommet de la pile.
- *     Avant de retirer l'élément, la fonction vérifie si la pile n'est pas vide.
+ *     Avant de retirer l'élément, la fonction vérifie 
+ * si la pile n'est pas vide.
  *     Si la pile est vide, un message d'erreur est affiché sur stderr,
  *     et le programme se termine avec un statut EXIT_FAILURE.
  *     Si la pile contient au moins un élément, cet élément est retiré
  *     et la mémoire qu'il occupait est libérée.
- *     La fonction met ensuite à jour les pointeurs pour maintenir la structure de la pile.
+ *     La fonction met ensuite à jour les pointeurs
+ * pour maintenir la structure de la pile.
  */
 
 void pop(stack_t **stack, unsigned int line_number)
