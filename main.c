862fb9a52d338321009e6fd6d808043f8bfdd7e3
SpeedCash <tcravero@gmail.com>
2024-01-04 08:31:29 +0000

    updated main

--- main.c
+++ main.c
@@ -66,9 +66,13 @@ int main(int argc, char *argv[])
 		{
 			pop(&stack, line_number);
 		}
-		else if (strcmp(opcode, "swap") == 0) // Ajout de la condition pour 'swap'
+		else if (strcmp(opcode, "swap") == 0)
 		{
 			swap(&stack, line_number);
+		}
+		else if (strcmp(opcode, "add") == 0) // Ajout de la condition pour 'add'
+		{
+			add(&stack, line_number);
 		}
 		else
 		{
