99803fd61d6a74a1ae3b67d7f6edc346674f0a17
harishammache <hammache_haris@hotmail.fr>
2024-01-04 12:00:25 +0000

    Implement the push and pall opcodes

--- pall.c
+++ pall.c
@@ -10,15 +10,13 @@
  * file where the "pall" opcode is encountere
  */
 
-void pall(stack_t **stack, unsigned int line_number)
+void pall(stack_t *stack, unsigned int line_number)
 {
-	stack_t *current = *stack;
+	(void) line_number;
 
-	(void)line_number;
-
-	while (current != NULL)
+	while (stack != NULL)
 	{
-		printf("%d\n", current->n);
-		current = current->next;
+		printf("%d\n", stack->n);
+		stack = stack->next;
 	}
 }
